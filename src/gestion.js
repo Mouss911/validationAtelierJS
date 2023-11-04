2e4e54b1feda37652f2962c12fef94a3d19a9689
Santos25 <abdoulayejuniorsantos@gmail.com>
2023-11-04 20:26:53 +0100

    associe

---
+++ src/gestion.js
@@ -0,0 +1,75 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCSRo2EZwo5LQIO75FevIBvEKbDD61HNuY",
+    authDomain: "validation-atelier-js.firebaseapp.com",
+    databaseURL: "https://validation-atelier-js-default-rtdb.firebaseio.com",
+    projectId: "validation-atelier-js",
+    storageBucket: "validation-atelier-js.appspot.com",
+    messagingSenderId: "466332062090",
+    appId: "1:466332062090:web:ffbe45ef4a7371a7b5b873"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+
+
+// const entreprise = collection(db, "entreprise");
+const associe = collection(db, "associe");
+const container = document.querySelector('.logo-entreprise')
+onSnapshot(associe, (snapshot) => {
+    let associe = [];
+    snapshot.docs.forEach((doc) => {
+        associe.push({...doc.data(), id: doc.id});
+    })
+    console.log(associe);
+    associe.forEach((ins) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <button type="button" class="btn btn-dark text-warning">logo</button>
+        <p >${ins.nom}</p>    
+        `
+        container.appendChild(div);
+    });
+})
+
+// console.log(associe);
+// getDocs(associe).then((snapshat) => {
+//     let associe = [];
+//     snapshat.docs.forEach((doc) => {
+//         associe.push({...doc.data(), id: doc.id});
+//     });
+//     // console.log(associe);
+//     associe.forEach((ins) => {
+//         const div = document.createElement('div');
+//         div.innerHTML = `
+//         <button type="button" class="btn btn-dark text-warning">logo</button>
+//         <p >${ins.nom}</p>    
+//         `
+//         container.appendChild(div);
+//     });
+// });
+
+
+const addToFirebase = document.querySelector(".addToFirebase");
+console.log(addToFirebase);
+addToFirebase.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    
+    addDoc(associe, {
+        nom : addToFirebase.nom.value,
+        coordonnee : addToFirebase.coordonne.value,
+        email : addToFirebase.email.value
+    }).then(() => addToFirebase.reset());
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  devtool: "eval-cheap-source-map",
-  watch: true,
+    mode: "development",
+    entry: ["./src/gestion.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    devtool: "eval-cheap-source-map",
+    watch: true,
 };
