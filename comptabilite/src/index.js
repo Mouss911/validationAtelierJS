067228cb75b84c0f9b2cc1ffffce984ec88bd7d6
“Mbaye <seckm105@gmail.com>
2023-11-07 12:18:29 +0000

    Debug

--- comptabilite/src/index.js
+++ comptabilite/src/index.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-
 // Importation des  services
 import {
   addDoc,
@@ -32,7 +31,7 @@ const eleve = collection(db, 'inscScolarite');
 getDocs(eleve).then((snapshot) => {});
 
 // Realtime Update
-onSnapshot(eleve.orderBy('dateDajout', 'desc'), (snapshot) => {
+onSnapshot(eleve, (snapshot) => {
   let eleve = [];
   snapshot.docs.forEach((doc) => {
     eleve.push({ ...doc.data(), id: doc.id });
@@ -53,41 +52,59 @@ onSnapshot(eleve.orderBy('dateDajout', 'desc'), (snapshot) => {
         </td>`;
     list.appendChild(tr);
   });
-  console.log(eleve);
 });
 
 //recuperer les données(nom, prenom, date) et les afficher dans revenue
 //partie pape cheikh
 
+// var database = firebase.database()
+
 onSnapshot(eleve, (snapshot) => {
   let eleve = [];
   snapshot.docs.forEach((doc) => {
     eleve.push({ ...doc.data(), id: doc.id });
+    // console.log(snapshot);
   });
-  const total = document.getElementById('total');
-  // console.log(eleve[0].etatFin);
-  revenue.innerHTML = '';
-  let totalEtatFin = 0;
   eleve.forEach((utili) => {
     const revenue = document.getElementById('revenue');
+    const total = document.getElementById('total');
+
     let trbody = document.createElement('tr');
+    let trfoot = document.createElement('tr');
+
     trbody.innerHTML = `
-    <td class="border border-1">date</td>
-    <td class="text-center">${utili.type}</td>
-    <td class="text-center border border-1">${utili.prenom} ${utili.nom}</td>
-    <td class="border border-1">${utili.etatFin} Fcfa</td>
+          <td class="border border-1">date</td>
+          <td class="text-center">${utili.type}</td>
+          <td class="text-center border border-1">${utili.prenom} ${utili.nom}</td>
+          <td class="border border-1">${utili.etatFin} Fcfa</td>
     `;
-    revenue.appendChild(trbody);
-    totalEtatFin += parseInt(utili.etatFin);
+    trfoot.innerHTML = `
+          <td colspan="3" class="border border-1"><b>TOTAL</b></td>
+          <td class="border border-1"> <b>45000 Fcfa</b></td>
+    `;
+
+    revenue.append(trbody);
+    total.append(trfoot);
   });
-  total.innerHTML = '';
-  let trfoot = document.createElement('tr');
-  trfoot.innerHTML = `
-  <td colspan="3" class="border border-1"><b>TOTAL</b></td>
-  <td class="border border-1"> <b>${totalEtatFin} Fcfa</b></td>
-  `;
-  total.appendChild(trfoot);
 });
+//recuperation types
+// Obtenez une référence à votre base de données Firestore
+// var db1 = firebase.firestore();
+
+// Utilisez la référence pour obtenir les données en fonction de l'id
+
+// console.log(eleve);
+
+// var type = "votre_id";
+// var collectionRef = db.collection("inscrireActivite");
+// var query = collectionRef.where("id", "==", id);
+
+// query.get().then(function(querySnapshot) {
+//   querySnapshot.forEach(function(doc) {
+//     // doc contient les données correspondantes à votre requête
+//     console.log(doc.data());
+//   });
+// });
 
 // Enregistrer des données dans le Firebase
 const form = document.querySelector('.addToFirebase');
